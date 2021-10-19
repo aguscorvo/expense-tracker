4b4e546b1b50fa7c86fdd716e9795e2d719425cd
aguscorvo <agustina@streaver.com>
2021-10-19 13:53:13 -0300

    Asignment 1

---
+++ src/components/Expenses.js
@@ -0,0 +1,19 @@
+import "./Expenses.css";
+import ExpenseItem from "./ExpenseItem";
+
+function Expenses(props) {
+  return (
+    <div className="expenses">
+      {props.expenses.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default Expenses;
