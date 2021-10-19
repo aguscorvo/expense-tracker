c37825242ab6db5ae2ea81cd3a0760c60deeebdc
aguscorvo <agustina@streaver.com>
2021-10-19 14:33:01 -0300

    Add wrapper component and concept of 'props.children'

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
-import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import "./Expenses.css";
 
 function Expenses(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       {props.expenses.map((expense) => (
         <ExpenseItem
           key={expense.id}
@@ -12,7 +13,7 @@ function Expenses(props) {
           date={expense.date}
         />
       ))}
-    </div>
+    </Card>
   );
 }
 
