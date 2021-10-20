0316614c206199a5c306b7743de4021c2aa0e658
aguscorvo <agustina@streaver.com>
2021-10-20 14:49:52 -0300

    Add submit handler

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -26,7 +26,7 @@ const ExpenseForm = () => {
   };
 
   const amountChangeHandler = (event) => {
-    setEnteredAmount(event.targe.value);
+    setEnteredAmount(event.target.value);
     // setUserInput({
     //   ...userInput,
     //   enteredAmount: event.target.value,
@@ -41,8 +41,18 @@ const ExpenseForm = () => {
     // });
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
