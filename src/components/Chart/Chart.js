815eb9e6dc6fafffff7aef77e707f5369c745749
aguscorvo <agustina@streaver.com>
2021-10-22 10:50:58 -0300

    Add Chart with Chartbars to Expenses component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,10 +3,18 @@ import ChartBar from "./ChartBar";
 import "./Chart.css";
 
 const Chart = (props) => {
+  const valueArray = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaximum = Math.max(...valueArray);
+
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
-        <ChartBar key={dataPoint.label} value={dataPoint.value} maxValue={null} label={dataPoint.label}/>
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          maxValue={totalMaximum}
+          label={dataPoint.label}
+        />
       ))}
     </div>
   );
